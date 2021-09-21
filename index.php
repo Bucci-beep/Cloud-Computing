e55f98506ada59e176119f5dfa17ef216afdd081
Bucci-beep <66949089+Bucci-beep@users.noreply.github.com>
2021-09-21 04:35:34 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+//echo 'hello world';
+$stringOne = 'my name is';
+$stringTwo = 'Denis Kibuchi';
+//echo $stringOne.$stringTwo;
+
+$name = 'Kibuchi';
+
+//echo 'Hey, my name is ' . $name;
+
+//echo "the ninja screamed \"whaaaa\"";
+//echo 'the ninjas screamed "whaaa"';
+//finding characters
+//echo $name[0];
+
+//functions
+//echo strlen($name); >> gets the length of the variable 
+//echo strtoupper($name); >> capitalises the variable 
+//echo str_replace('b', 'f', $name); >> replaces characters
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>my first PHP file</title>
+    </head>
+    <body>
+
+    </body>
+</html>
